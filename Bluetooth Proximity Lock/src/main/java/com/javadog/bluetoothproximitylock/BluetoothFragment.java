2789339187bf8c0ebc5941f03555e0213c7d8725
Collie <culmor30@gmail.com>
2014-05-24 15:38:28 -0400

    Added README.md and Apache license.

--- Bluetooth Proximity Lock/src/main/java/com/javadog/bluetoothproximitylock/BluetoothFragment.java
+++ Bluetooth Proximity Lock/src/main/java/com/javadog/bluetoothproximitylock/BluetoothFragment.java
@@ -1,3 +1,19 @@
+/*
+	Copyright 2014 Cullin Moran
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	    http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
 package com.javadog.bluetoothproximitylock;
 
 import android.bluetooth.BluetoothDevice;
@@ -5,7 +21,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.LocalBroadcastManager;
@@ -15,7 +30,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.widget.Switch;
---
+++ README.md
@@ -0,0 +1,12 @@
+# Bluetooth Proximity Lock
+- - -
+
+This app runs a service in the background which monitors the approximate physical distance of bluetooth peripherals from the user's device. Should the bluetooth peripheral go out of range, the user's device lockscreen will be enabled. When in range, the device will have its lockscreen disabled for easy access.
+
+This app will be published on Google Play for a low price when it reaches a more complete state, but it will contain no license-checking. It will also remain open-source (under the Apache license) and freely-available for those who wish to compile it for themselves; all I ask is that users please not distribute compiled packages.
+
+Planned features:
+
+- [ ] Lock device when bluetooth peripheral leaves user-defined radius (approximate).
+- [ ] Keep device unlocked when it is connected to a user-specified "trusted" WiFi network.
+- [ ] Save battery by only running the service right as the device screen is woken up.
